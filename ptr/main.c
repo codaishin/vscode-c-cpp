0842ea010c680ae99bb6276e7bfe2c821d4e2b68
codaishin <codaishin@gmail.com>
2021-10-31 16:51:44 +0100

    update: ptr

--- ptr/main.c
+++ ptr/main.c
@@ -1,48 +1,49 @@
+#include <malloc.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int _digits_count(int number) {
-	int count = 0;
-	do {
-		number /= 10;
+int _length(int number) {
+	int count = 1;
+	while (number /= 10) {
 		++count;
-	} while (number);
+	}
 	return count;
 }
 
-int _pow(int base, int power) {
-	int result = 1;
-	for (int i = 0; i < power; ++i) {
-		result *= base;
+int _make_absolute(int *number) {
+	if (*number < 0) {
+		*number *= -1;
+		return 1;
 	}
-	return result;
-}
-
-void _write_number(int number, int length, char *string) {
-	int digit, div;
-	int rest = number;
-	for (int exp = length - 1, i = 0; exp >= 0; --exp, ++i) {
-		div = _pow(10, exp);
-		digit = rest / div;
-		rest = number % div;
-		string[i] = '0' + digit;
-	}
-	string[length] = '\0';
+	return 0;
 }
 
 char *_malloc(int length) {
-	return (char *)malloc((length + 1) * sizeof(char));
+	char *result = (char *)malloc((length + 1) * sizeof(char));
+	result[0] = '-';
+	return result;
+}
+
+void _write_number(char *target, int source, int length) {
+	target[length--] = '\0';
+	for (; source > 0; source /= 10) {
+		target[length--] = '0' + source % 10;
+	}
 }
 
 char *int_to_string(int number) {
-	int length = _digits_count(number);
+	int length = _make_absolute(&number) + _length(number);
 	char *result = _malloc(length);
-	_write_number(number, length, result);
+	_write_number(result, number, length);
 	return result;
 }
 
 int main() {
-	char *z = int_to_string(999444);
-	printf("%s\n", z);
-	free(z);
+	int numbers[] = {7, 42, -57};
+	int count = sizeof(numbers) / sizeof(int);
+
+	for (int i = 0; i < count; ++i) {
+		char *as_string = int_to_string(numbers[i]);
+		printf("%s\n", as_string);
+		free(as_string);
+	}
 }
